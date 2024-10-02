e77c4807d7a124141bfbc5273669215b7cb38745
Ruhit Baidya <ruhitbaidya01@gmail.com>
2024-10-03 00:18:31 +0600

    login user complate

--- Controler/login.control.js
+++ Controler/login.control.js
@@ -1,8 +1,26 @@
+const registerModel = require("../model/register.model")
 
 
 
 const login = async(req, res)=>{
-    
+    console.log(req.body)
+    try{
+      const {email, password} = req.body
+      const findUser = await registerModel.findOne({email})
+      console.log(findUser)
+      if(findUser){
+        if(findUser.password === password){
+          res.send({message : "Successfully Login"})
+        }else{
+          res.send({message : "Authantaction Failed!"})
+        }
+      }else{
+        res.send({message : "Authantaction Failed!"})
+      }
+    }
+    catch(err){
+      res.send({message : err.message})
+    }
   }
 
 module.exports = login
--- router/authRouter.js
+++ router/authRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const register = require('../Controler/register.control');
 const tokenVerify = require('../tokengen/tokenVerify');
 const verifyOtpUser = require('../controler/UserVerifyOtp');
+const login = require('../Controler/login.control');
 
 const router = express.Router();
 
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post('/register', register)
 router.post('/verifyCheck', tokenVerify)
 router.post('/verifyUser', verifyOtpUser)
+router.post('/login', login)
 module.exports = router
